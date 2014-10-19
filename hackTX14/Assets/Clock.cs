c405c5c0f86f5ac6a3928b76a9eecf540f467f76
drt291 <dustin@Dustins-MacBook-Pro.local>
2014-10-18 20:22:40 -0500

    Clock

---
+++ hackTX14/Assets/Clock.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Clock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<TextMesh> ().text = string.Format("{0:hh:mm:ss tt}", DateTime.Now);
+
+	}
+
+}
