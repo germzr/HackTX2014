dacbefbde2b9e4d4d63364fea8874b6d7dc940cb
German Zuniga <germzr@gmail.com>
2014-10-18 21:32:43 -0500

    Added speech to text assets

---
+++ hackTX14/Assets/SpeakNow/SpeechToText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeechToText : MonoBehaviour {
+
+	private static AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer"); 
+	private static AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity"); 
+	public static string result="Empty";
+	public static void StartSpeech()
+	{
+		jo.Call("Launch");
+	}
+	public static string SpeechResult()
+	{
+		result=jo.CallStatic<string>("ReturnString");
+		return result;
+	}
+	public static void MakeToast(string toastmsg)
+	{
+		jo.Call("makeToast",toastmsg);
+	}
+
+}
---
+++ hackTX14/Assets/SpeakNow/test.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+	public static string tests="";
+	void OnGUI () 
+	{
+		if (Application.platform == RuntimePlatform.Android)
+		{
+			if(GUI.Button(new Rect(Screen.width/2,200,200,150),"SpeechToText"))
+			{
+			SpeechToText.StartSpeech();
+
+			}
+			tests=SpeechToText.SpeechResult();
+			GUI.Label(new Rect(Screen.width/2,590,200,200),tests);
+
+			if(GUI.Button(new Rect(Screen.width/2,400,200,150),"Toast Message"))
+			{
+			SpeechToText.MakeToast("Logging in.....Please Wait for a moment");			
+			}
+		}else
+			{
+
+
+
+		GUI.Label(new Rect(Screen.width/2,Screen.height/2,300,200),"Please Build and Run in Android and see documentation for usage of functionalities.");
+			}
+	}
+
+}
