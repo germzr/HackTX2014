0e9eeecee7329c2b9be43fa1213694fbb0669ae2
Casey Richardson <caseybrichardson@icloud.com>
2014-10-19 03:32:23 -0500

    Fixed my reddit stuff and put a test in my scene.

---
+++ hackTX14/Assets/Casey Stuff/Scripts/RedditPicturePost.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedditPicturePost : MonoBehaviour {
+
+	public Material ImageMat;
+	public TextMesh Text;
+	
+	void Start () {
+	}
+
+	public void FindReferences() {
+		ImageMat = transform.FindChild("Image").renderer.material;
+		Text = transform.FindChild("Title").GetComponent<TextMesh>();
+	}
+
+	void Update () {
+	
+	}
+}
