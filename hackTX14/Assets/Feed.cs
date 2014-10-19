f284e30eb640982488cfb5fb22aabb90e02284a0
Jacob Figueroa <jacobx04@hotmail.com>
2014-10-18 20:01:08 -0500

    Added a Feed and Post class

---
+++ hackTX14/Assets/Feed.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Feed {
+
+	public Post post;
+
+	public Feed(Post p)
+	{
+		post = p;
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//get updated feed
+		//post = the updated info...
+	}
+}
---
+++ hackTX14/Assets/Post.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Post {
+	public string message;
+	public string[] link;
+	public string description;
+
+
+	Post(string m, string[] l, string d)
+	{
+		message = m;
+		link = l;
+		description = d;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
