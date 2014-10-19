7abc19a1722e9b0f0e532594cc1598ba3ff074ee
Jacob Figueroa <jacobx04@hotmail.com>
2014-10-19 00:32:32 -0500

    CalendarManager parses calendar.json

---
+++ hackTX14/Assets/CalendarEvent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+
+public class CalendarEvent {
+	public string title;
+	public string description;
+	public string startingDate;
+
+	public CalendarEvent(string t, string d, string sd) {
+		title = t;
+		description = d;
+		startingDate = sd;
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
